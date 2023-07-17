de9f3e5b6664378a3d3abc0231cd3015cf6fe5b9
asaqa988 <asaqa001@gmail.com>
2023-07-17 21:08:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
 function App() {
+  const [myArray, setMyArray] = useState([]);
+
+  useEffect(() => {
+    FetchingTheData();
+  }, []);
+
+  const FetchingTheData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json(); // Add return statement here
+      })
+      .then((data) => {
+        setMyArray(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {myArray.map((userInformation, index) => {
+        return (
+          <div key={index}>
+            <h1> {userInformation.name}</h1>
+            <h3> User ID is : {userInformation.id}</h3>
+            <ul>
+              Adresses information : {userInformation.address.city}
+              <li>{userInformation.address.street}</li>
+              <li>{userInformation.address.suite}</li>
+              <li>{userInformation.address.zipcode}</li>
+            </ul>
+            <b>
+              {" "}
+              <i> {userInformation.email}</i>
+            </b>
+            <p>
+              {" "}
+              <b>Company Information </b>
+            </p>
+            <b>name is : {userInformation.company.name}</b>
+            <br /> <br />
+            <b>{userInformation.company.catchPhrase}</b>
+            <br /> <br />
+            <b>{userInformation.company.bs}</b>
+            <br /> <br />
+          </div>
+        );
+      })}
     </div>
   );
 }
